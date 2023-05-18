13d9617b93d6a44a34278aea66cfbad572080d6c
Viktor Mikulášek <mikulasek.viktor.cz@gmail.com>
2023-05-18 23:37:42 +0200

    Hibernate

--- HibernateAndSpringJPA/databases/src/test/java/cz/vibri/databases/MySQLIntegrationTest.java
+++ HibernateAndSpringJPA/databases/src/test/java/cz/vibri/databases/MySQLIntegrationTest.java
@@ -1,45 +1,28 @@
 package cz.vibri.databases;
 
-import cz.vibri.databases.domain.Book;
 import cz.vibri.databases.repositories.BookRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("local")
 @DataJpaTest
 @ComponentScan(basePackages = {"cz.vibri.databases.bootstrap"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class SpringBootJpaTestSlice {
+public class MySQLIntegrationTest {
 
     @Autowired
     BookRepository bookRepository;
 
-    @Commit
-    @Order(1)
     @Test
-    void testJpaTestSlice() {
+    void testMySQL() {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(2);
-        bookRepository.save(new Book("My book", "123464", "Self"));
-
-        long countAfter = bookRepository.count();
-        assertThat(countBefore).isLessThan(countAfter);
-    }
-
-    @Order(2)
-    @Test
-    void testJpaTestSliceTransaction() {
-        long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(3);
 
     }
 }
